c5da8241f149b72123bf936dae682db83ac99b78
Utsav <utsav.s3280@gmail.com>
2022-12-19 20:24:19 +0530

    added backend

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+mongoose.connect(process.env.Mongo_URL, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true }, () => {
+    console.log('connected to DB')
+});
+
+mongoose.set('strictQuery', true);
+const app = express();
+app.use(express.json());
+
+app.listen(5000, () => console.log("Server is up at 5000"))
