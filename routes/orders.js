e4c4a0e438f3075b59cf52b8107ac4a646fab4cf
Srikant <csrikantprakash@gmail.com>
2022-12-21 18:00:00 +0530

    added GET-POST APIs

--- routes/orders.js
+++ routes/orders.js
@@ -39,4 +39,39 @@ router.get("/", authentication, async (req, res) => {
     }
 }); 
 
+router.post("/id", authentication, async (req, res) => {
+    try {
+        console.log(req.body);
+        const orders = await Orders.find({ _id: req.body._id });
+        res.json({
+            orders
+        })
+        // return orders.OrderDetails
+    } catch (error) {
+        res.json({
+            message: error.message
+        })
+    }
+});
+
+
+router.post("/create", authentication, async (req, res) => {
+    try {
+        const order = await Orders.create({
+            userID: req.userID, //
+            _id: req.body._id,
+            items: req.body.items,
+            price: req.body.price,
+            OrderDetails: req.body.OrderDetails
+        });
+        res.json({
+            order: order
+        })
+    } catch (error) {
+        res.json({
+            message: error.message
+        })
+    }
+})
+
 module.exports = router;
