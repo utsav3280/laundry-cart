62996dc80b1b8829866138fdbfbcfb2096af87dc
Utsav <utsav.s3280@gmail.com>
2022-12-21 17:39:51 +0530

    Added GET APIs and Modals

--- models/orderModels.js
+++ models/orderModels.js
@@ -2,10 +2,9 @@ const mongoose = require("mongoose");
 const moment = require("moment");
 const Schema = mongoose.Schema;
 const ObjectId = Schema.ObjectId;
-const Users = require("./userModel");
 
 const OrderSchema = new Schema({
-    userId: { type: ObjectId, ref: "Users" },
+    userID: { type: ObjectId, ref: "Users" },
     _id: { type: String, unique: true },
     dateTime: { type: String, default: moment().format('lll') },
     items: { type: Number },
@@ -16,6 +15,3 @@ const OrderSchema = new Schema({
 
 const Orders = mongoose.model("Orders", OrderSchema);
 module.exports = Orders;
-
-
-
--- models/userModel.js
+++ models/userModel.js
@@ -1,17 +1,17 @@
 const mongoose = require("mongoose");
 
 const UserSchema = new mongoose.Schema({
-    name: { type: String },
-    email: { type: String, unique: true },
-    phone: { type: Number, unique: true },
-    password: { type: String },
+    name: { type: String, required: true},
+    email: { type: String, unique: true, required: true },
+    phone: { type: Number, unique: true, required: true },
+    password: { type: String, required: true },
     address: {
         street: { type: String },
         district: { type: String },
         state: { type: String },
         pincode: { type: String }
     }
-});
+}); 
 
 const Users = mongoose.model("UserDetails", UserSchema);
 module.exports = Users;
